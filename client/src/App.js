b18b523aab15c9b5522e3029e48970f89fd25398
LyubovTatur <71625191+LyubovTatur@users.noreply.github.com>
2021-11-21 17:25:24 +0300

    damn me = fullstack now??

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
+
 
 function App() {
+    const [id, setId] = useState('')
+    const [title, setTitle] = useState('')
+    const [budget, setBudget] = useState('')
+    const [dues, setDues] = useState('')
+    const [mark, setMark] = useState('')
+
+    const [films, setFilms] = useState([])
+
+    const getFilms = () => {
+        axios.get('http://localhost:3001/films',{
+
+        }).then((response)=>{
+            setFilms(response.data)
+        })
+    }
+
+    const AddFilm = () =>{
+        axios.post('http://localhost:3001/create',{
+            id:id,
+            title:title,
+            budget:budget,
+            dues:dues,
+            mark:mark
+
+        }).then(()=>{
+            setFilms([...films,{
+                id:id,
+                title:title,
+                budget:budget,
+                dues:dues,
+                mark:mark
+
+            }])
+            console.log('success')
+        })
+    }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>films</h1>
+
+      <div className={'column'}>
+        <label htmlFor="">id </label>
+        <input type="text" onChange={(e)=>{setId(e.target.value)}} value={id}/>
+      </div>
+      <div className={'column'}>
+        <label htmlFor="">title </label>
+        <input type="text"  onChange={(e)=>{setTitle(e.target.value)}} value={title}/>
+      </div>
+      <div className={'column'}>
+        <label htmlFor="">budget </label>
+        <input type="text"  onChange={(e)=>{setBudget(e.target.value)}} value={budget}/>
+      </div>
+      <div className={'column'}>
+        <label htmlFor="">dues </label>
+        <input type="text" onChange={(e)=>{setDues(e.target.value)}} value={dues}/>
+      </div>
+      <div className={'column'}>
+        <label htmlFor="">mark </label>
+        <input type="text" onChange={(e)=>{setMark(e.target.value)}} value={mark}/>
+      </div>
+        <button onClick={(AddFilm)}>Add</button>
+        <button onClick={(getFilms)}>Get</button>
+
+        <table>
+            <tr>
+                <td>айди</td>
+                <td>тайтл</td>
+                <td>баджет</td>
+                <td>дуес</td>
+                <td>марк</td>
+            </tr>
+        {films.map((value,key) =>{
+            return (
+            <tr>
+                <td>{value.id}</td>
+                <td>{value.title}</td>
+                <td>{value.budget}</td>
+                <td>{value.dues}</td>
+                <td>{value.mark}</td>
+            </tr>
+            )
+        })}
+        </table>
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express()
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json())
+const db = mysql.createConnection({
+    user:'root',
+    host:'localhost',
+    password:'1111',
+    database:'db_films'
+})
+
+app.post('/create',(req,res)=>{
+    const id = req.body.id
+    const title = req.body.title
+    const budget = req.body.budget
+    const dues = req.body.dues
+    const mark = req.body.mark
+
+    db.query('insert into films values (?,?,?,?,?)',
+        [id,title,budget,dues,mark],
+        (err,result) =>{
+            if (err){
+                console.log(err)
+            }
+            else{
+                res.send("values inserted")
+            }
+        })
+
+
+
+})
+
+
+app.get('/films',(req,res)=>{
+
+    db.query('select * from films',
+        (err,result) =>{
+            if (err){
+                console.log(err)
+            }
+            else{
+                res.send(result)
+            }
+        })
+
+
+
+})
+
+
+
+app.listen(3001,()=> {
+    console.log("server start on port 3001 ")
+})
